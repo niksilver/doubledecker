1d86fe5dd42db3904c3fa0a73318966905fbf5fe
Nik Silver <nik@niksilver.com>
2023-06-07 13:40:37 +0100

    Small refactor.

--- lib/touchosc.lua
+++ lib/touchosc.lua
@@ -1,5 +1,7 @@
 local TouchOSC = {} function TouchOSC:init(bind)
+    -- Capture the binding module
     self.bind = bind
+
     self.bind:add_listener(function(page, row, col, layer, normalized)
         print('/' .. page .. '/' .. row .. '/' .. col .. '/' .. layer .. " -> " .. normalized)
     end)
@@ -9,28 +11,33 @@ end
 -- Format of the path will be `/doubledecker/page/row/col/layer`.
 --
 function TouchOSC:osc_event(path, args, from)
-    local layer = 1
     local page, row, col, layer = string.match(path, '/doubledecker/(%d+)/(%d+)/(%d+)/(%d+)')
     print("\n\nosc.event.path = " .. path)
     if page and row and col and layer then
-        local val = args[1]
-        print("osc.event val = " .. tostring(val))
-        local b = self.bind:get(page, row, col, layer)
-        print("osc.event: b = " .. TouchOSC.dump(b))
-        -- print("osc.event: b.param = " .. TouchOSC.dump(b.param))
-        print("            tOPTION = " .. params.tOPTION)
-        if b.param and val then
-            -- See if we can normalize the value from TouchOSC
-            local normalized = val
-            if b.param.t == params.tOPTION then
-                -- An option parameter
-                normalized = val / (b.param.count - 1)
-            end
+        self:control_event(page, row, col, layer, args, from)
+    end
+end
 
-            b:set(normalized)
-            screen_dirty = true
-            print("osc.event.path: Updated to normalized val = " .. normalized)
+-- Respond to a control change from TouchOSC
+--
+function TouchOSC:control_event(page, row, col, layer, args, from)
+    local val = args[1]
+    print("osc.event val = " .. tostring(val))
+    local b = self.bind:get(page, row, col, layer)
+    print("osc.event: b = " .. TouchOSC.dump(b))
+    -- print("osc.event: b.param = " .. TouchOSC.dump(b.param))
+    print("            tOPTION = " .. params.tOPTION)
+    if b.param and val then
+        -- See if we can normalize the value from TouchOSC
+        local normalized = val
+        if b.param.t == params.tOPTION then
+            -- An option parameter
+            normalized = val / (b.param.count - 1)
         end
+
+        b:set(normalized)
+        screen_dirty = true
+        print("osc.event.path: Updated to normalized val = " .. normalized)
     end
 end
 
